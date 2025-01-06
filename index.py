78335925022a2cbefa6164f339dde9235acd598f
jacktone owidi <125829691+jacktoneowidi@users.noreply.github.com>
2025-01-06 04:53:03 -0800

    Add files via upload

---
+++ index.py
@@ -0,0 +1,25 @@
+# print('''world hello''')
+
+
+
+# price = 10 + 20
+# rating = 4.9
+# name = 'jacktone'
+# is_jacktone = False 
+# print (price)
+
+
+# print ('welcome to our onestop hospital')
+# name = 'john smith'
+# age = 20 
+# is_new = True
+
+
+# name = input('what is your name?')
+# print('hii ', name)
+
+
+
+name = input ('whats your name ? ')
+favourite_color = input('whats your favourite color ? ')
+print(name , ' likes color ', favourite_color)
