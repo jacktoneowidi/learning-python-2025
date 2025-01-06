7193b683467eb85f3010575ac05263c5613c5c4c
jacktone owidi <125829691+jacktoneowidi@users.noreply.github.com>
2025-01-06 04:58:19 -0800

    Update index.py

--- index.py
+++ index.py
@@ -1,6 +1,9 @@
 
 
+#learn about input function in python
 
+#ask two questions persons name and favourite color,
+#then print a messege like 'jack likes red'
 name = input ('whats your name ? ')
 favourite_color = input('whats your favourite color ? ')
 print(name , ' likes color ', favourite_color)
