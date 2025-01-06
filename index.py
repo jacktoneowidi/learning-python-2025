7a25d52e1203725a3dacaecf8e52fe094f657cd4
jacktone owidi <125829691+jacktoneowidi@users.noreply.github.com>
2025-01-06 04:53:33 -0800

    Update index.py

--- index.py
+++ index.py
@@ -1,22 +1,3 @@
-# print('''world hello''')
-
-
-
-# price = 10 + 20
-# rating = 4.9
-# name = 'jacktone'
-# is_jacktone = False 
-# print (price)
-
-
-# print ('welcome to our onestop hospital')
-# name = 'john smith'
-# age = 20 
-# is_new = True
-
-
-# name = input('what is your name?')
-# print('hii ', name)
 
 
 
