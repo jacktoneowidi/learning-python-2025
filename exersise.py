4354a581f158113d600806066e537879da629b0d
jacktone owidi <125829691+jacktoneowidi@users.noreply.github.com>
2025-01-07 05:03:30 -0800

    Update exersise.py

--- exersise.py
+++ exersise.py
@@ -1,1 +1,10 @@
 
+# input - ask a user a question
+# int - convert a string into an integer 
+# float - converting a string into a number with decimal
+# bool - converting a string into a boolean value
+
+
+user = input ('which year were you born in')
+age =   2019 - int(user)
+print (age)
